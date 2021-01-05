b186828e6f64e8c5ba2de5f33b3187ca940ab561
PC-FRAN\Fran <frandiaz1979@gmail.com>
2021-01-05 21:01:55 +0100

    Agregada la lógica de negocio al dominio, con clean code.

--- api/webapi/Controllers/DrinkController.cs
+++ api/webapi/Controllers/DrinkController.cs
@@ -1,23 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using models;
-using services;
-
 namespace webapi.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using models;
+    using services;
+    using System.Collections.Generic;
+
     [ApiController]
     [Route("[controller]")]
     public class DrinkController : ControllerBase
     {
-        // private static readonly string[] Summaries = new[]
-        // {
-        //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        // };
-
         private readonly ILogger<DrinkController> _logger;
 
         public DrinkController(ILogger<DrinkController> logger)
@@ -28,18 +20,17 @@ namespace webapi.Controllers
         [HttpGet]
         public IEnumerable<Drink> Get()
         {
-            // return Enumerable.Range(1, 5).Select(index => new Drink
-            // {
-            //     Date = DateTime.Now.AddDays(index),
-            //     TemperatureC = rng.Next(-20, 55),
-            //     Summary = Summaries[rng.Next(Summaries.Length)]
-            // })
-            // .ToArray();
-
             var drinkService = new DrinkService();
-            var drinks = drinkService.ReadAll();
-            return drinks;
-            
+            var result = drinkService.ReadAll();
+            return result;
+        }
+
+        [HttpPost]
+        public string Post(string drinkType, float money, int sugars, int extraHot)
+        {
+            var drinkService = new DrinkService();
+            var result = drinkService.Pedir(drinkType, money, sugars, extraHot);
+            return result;
         }
     }
 }
--- domain/services/DrinkService.cs
+++ domain/services/DrinkService.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using models;
-using repositories;
-
-namespace services
+﻿namespace services
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using models;
+    using repositories;
+
+    //TODO: Quitar magic strings
     public class DrinkService
     {
-        List<DrinkPrice> drinkPrices = new List<DrinkPrice> 
+        List<DrinkPrice> drinkPrices = new List<DrinkPrice>
         {
             new DrinkPrice{ Price = 0.4F, Name = "Tea" },
             new DrinkPrice{ Price = 0.5F, Name = "Coffee" },
@@ -17,8 +19,6 @@ namespace services
 
         public IEnumerable<Drink> ReadAll()
         {
-            //IEnumerable<Drink> result;
-
             var orderRepository = new OrderRepository();
             var orders = orderRepository.ReadAll();
 
@@ -26,13 +26,71 @@ namespace services
             {
                 var drink = new Drink
                 {
-                    DrinkType = drinkPrices.Where(x=>x.Name==order.DrinkType).Single().Name,                    
+                    DrinkType = drinkPrices.Where(x => x.Name == order.DrinkType).Single().Name,
                     Money = drinkPrices.Where(x => x.Name == order.DrinkType).Single().Price,
                     Sugars = order.Sugars,
                     ExtraHot = order.ExtraHot,
                 };
                 yield return drink;
             }
+        }
+
+        public string Pedir(string drinkType, float money, int sugars, int extraHot)
+        {
+            if (!ComprobarQueExisteBebida(drinkType))
+            {
+                return "The drink type should be tea, coffee or chocolate.";
+            }
+
+            if (!ComprobarQueHayaSuficienteDinero(drinkType, money))
+            {
+                return String.Format("The {0} costs {1}.",
+                    drinkPrices.Where(x => x.Name.ToLower() == drinkType.ToLower()).Single().Name,
+                    drinkPrices.Where(x => x.Name.ToLower() == drinkType.ToLower()).Single().Price.ToString("N", new CultureInfo("en-US")));
+            }
+
+            if (!ComprobarAzucarillos(sugars))
+            {
+                return "The number of sugars should be between 0 and 2.";
+            }
+
+            //TODO: OrderRepository.Add(...);
+
+            return GenerarFraseSalida(drinkType, extraHot, sugars);
+        }
+
+        private static string GenerarFraseSalida(string drinkType, int extraHot, int sugars)
+        {
+            String result;
+            result = String.Format("You have ordered a {0}", drinkType.ToLower());
+
+            if (extraHot > 0)
+            {
+                result += " extra hot";
+            }
+
+            if (sugars > 0)
+            {
+                result += String.Format(" with {0} sugars(stick included).", sugars);
+            }
+
+            return result;
+        }
+
+        private static bool ComprobarAzucarillos(int sugars)
+        {
+            return sugars >= 0 && sugars <= 2;
+        }
+
+        private bool ComprobarQueHayaSuficienteDinero(string drinkType, float money)
+        {
+            return money >= drinkPrices.Where(x => x.Name.ToLower() == drinkType.ToLower()).Single().Price;
+        }
+
+        private bool ComprobarQueExisteBebida(string drinkType)
+        {
+            if (drinkType == null) return false;
+            return drinkPrices.Where(x => x.Name.ToLower() == drinkType.ToLower()).Any();
         }
     }
 }
