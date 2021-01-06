378e4d204117b497b69cd94c5c00caeb8177a91a
PC-FRAN\Fran <frandiaz1979@gmail.com>
2021-01-06 13:46:47 +0100

    Agregadas pruebas unitarias

--- tests/tests-domain/DrinkServiceTests.cs
+++ tests/tests-domain/DrinkServiceTests.cs
@@ -1,30 +1,16 @@
-﻿namespace DomainTests
+﻿// TODO: Hacer mocks de capa servicio
+
+namespace DomainTests
 {
     using Models;
     using NUnit.Framework;
     using Services;
+    using System.Linq;
 
     ////List prices
 
-    ////|Drink|Price|
-    ////|---|---|
-    ////|Tea|0.4|
-    ////|Coffee|0.5|
-    ////|Chocolate|0.6|
 
     ////Validations
-    ////* If the drink type is not* tea*, * coffee* or * chocolate*, it shows the following message:
-    ////```
-    ////The drink type should be tea, coffee or chocolate.
-    ////```
-    ////* If the amount of money does not reach the price of the drink, a message as the following is displayed:
-    ////```
-    ////The tea costs 0.4.
-    ////```
-    ////* If the number of sugars is not between 0 and 2, it shows a message like this:
-    ////```
-    ////The number of sugars should be between 0 and 2.
-    ////```
     ////* If the arguments are right, the displayed message is:
     ////```
     ////You have ordered a coffee
@@ -96,6 +82,157 @@
 
             // Assert
             Assert.AreEqual("The drink type should be tea, coffee or chocolate.", resultado);
+        }
+
+        [TestCase("TEA", 0.39F, 1, 1)]
+        [TestCase("COFFEE", 0.49F, 1, 1)]
+        [TestCase("CHOCOLATE", 0.59F, 1, 1)]
+        public void DrinkServicePedir_DineroInsuficiente(string drinkType, float money, int sugars, int extraHot)
+        {
+            // Arrange
+            var drinkService = new DrinkService();
+            string resultado;
+            var drink = new Drink
+            {
+                DrinkType = drinkType,
+                Money = money,
+                Sugars = sugars,
+                ExtraHot = extraHot,
+            };
+
+            // Act
+            resultado = drinkService.Pedir(drink);
+
+            // Assert
+            Assert.AreEqual(
+                string.Format(
+                    "The {0} costs {1}.",
+                    drinkService.drinkPrices.Single(x => x.Name.ToLower() == drink.DrinkType.ToLower()).Name,
+                    drinkService.drinkPrices.Single(x => x.Name.ToLower() == drink.DrinkType.ToLower()).Price.ToString("N", new System.Globalization.CultureInfo("en-US"))
+                ),
+                resultado
+            );
+        }
+
+        [TestCase("COFFEE", 1F, -1, 1)]
+        [TestCase("TEA", 1F, 4, 1)]
+        [TestCase("CHOCOLATE", 1F, 2000, 1)]
+        public void DrinkServicePedir_CantidadAzucarillosMal(string drinkType, float money, int sugars, int extraHot)
+        {
+            // Arrange
+            var drinkService = new DrinkService();
+            string resultado;
+            var drink = new Drink
+            {
+                DrinkType = drinkType,
+                Money = money,
+                Sugars = sugars,
+                ExtraHot = extraHot,
+            };
+
+            // Act
+            resultado = drinkService.Pedir(drink);
+
+            // Assert
+            Assert.AreEqual("The number of sugars should be between 0 and 2.", resultado);
+        }
+
+        [TestCase("COFFEE", 1F, 0, 0)]
+        [TestCase("TEA", 1F, 0, 0)]
+        [TestCase("CHOCOLATE", 1F, 0, 0)]
+        public void DrinkServicePedir_BebidaFriaSinAzucar(string drinkType, float money, int sugars, int extraHot)
+        {
+            // Arrange
+            var drinkService = new DrinkService();
+            string resultado;
+            var drink = new Drink
+            {
+                DrinkType = drinkType,
+                Money = money,
+                Sugars = sugars,
+                ExtraHot = extraHot,
+            };
+
+            // Act
+            resultado = drinkService.Pedir(drink);
+
+            // Assert
+            Assert.AreEqual(string.Format("You have ordered a {0}", drink.DrinkType.ToLower()), resultado);
+        }
+
+        [TestCase("COFFEE", 1F, 1, 0)]
+        [TestCase("TEA", 1F, 1, 0)]
+        [TestCase("CHOCOLATE", 1F, 1, 0)]
+        [TestCase("COFFEE", 1F, 2, 0)]
+        [TestCase("TEA", 1F, 2, 0)]
+        [TestCase("CHOCOLATE", 1F, 2, 0)]
+        public void DrinkServicePedir_BebidaFriaConAzucar(string drinkType, float money, int sugars, int extraHot)
+        {
+            // Arrange
+            var drinkService = new DrinkService();
+            string resultado;
+            var drink = new Drink
+            {
+                DrinkType = drinkType,
+                Money = money,
+                Sugars = sugars,
+                ExtraHot = extraHot,
+            };
+
+            // Act
+            resultado = drinkService.Pedir(drink);
+
+            // Assert
+            Assert.AreEqual(string.Format("You have ordered a {0} with {1} sugars(stick included).", drink.DrinkType.ToLower(), drink.Sugars), resultado);
+        }
+
+        [TestCase("COFFEE", 1F, 0, 1)]
+        [TestCase("TEA", 1F, 0, 1)]
+        [TestCase("CHOCOLATE", 1F, 0, 1)]
+        public void DrinkServicePedir_BebidaCalienteSinAzucar(string drinkType, float money, int sugars, int extraHot)
+        {
+            // Arrange
+            var drinkService = new DrinkService();
+            string resultado;
+            var drink = new Drink
+            {
+                DrinkType = drinkType,
+                Money = money,
+                Sugars = sugars,
+                ExtraHot = extraHot,
+            };
+
+            // Act
+            resultado = drinkService.Pedir(drink);
+
+            // Assert
+            Assert.AreEqual(string.Format("You have ordered a {0} extra hot", drink.DrinkType.ToLower()), resultado);
+        }
+
+        [TestCase("COFFEE", 1F, 1, 1)]
+        [TestCase("TEA", 1F, 1, 1)]
+        [TestCase("CHOCOLATE", 1F, 1, 1)]
+        [TestCase("COFFEE", 1F, 2, 1)]
+        [TestCase("TEA", 1F, 2, 1)]
+        [TestCase("CHOCOLATE", 1F, 2, 1)]
+        public void DrinkServicePedir_BebidaCalienteConAzucar(string drinkType, float money, int sugars, int extraHot)
+        {
+            // Arrange
+            var drinkService = new DrinkService();
+            string resultado;
+            var drink = new Drink
+            {
+                DrinkType = drinkType,
+                Money = money,
+                Sugars = sugars,
+                ExtraHot = extraHot,
+            };
+
+            // Act
+            resultado = drinkService.Pedir(drink);
+
+            // Assert
+            Assert.AreEqual(string.Format("You have ordered a {0} extra hot with {1} sugars(stick included).", drink.DrinkType.ToLower(), drink.Sugars), resultado);
         }
     }
 }
